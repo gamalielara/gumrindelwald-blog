768d96005e2e71e2e3f67ac8ec927db567cf99f2
gamalielara <aragamalielboanerges@gmail.com>
2023-06-11 01:14:28 +0700

    blurry skeleton video loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,34 @@ import HeadDocument from "../components/HeadDocument";
 import HeadingThree from "../components/text/HeadingThree";
 import Link from "next/link";
 import Logo from "../components/logo/Logo";
+import styles from "./index.module.css";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (!videoRef.current) return;
+
+    const defaultClassnames =
+      "w-full h-full object-cover absolute top-0 right-0 z-10";
+
+    if (videoRef.current?.readyState === 4) {
+      videoRef.current.className = defaultClassnames;
+    } else {
+      videoRef.current.className += " " + styles["video-blur"];
+    }
+
+    // videoRef.current.addEventListener("loadstart", () => {
+
+    // })
+  }, []);
+
   return (
     <>
       <HeadDocument isLandingPage />
       <main className="w-screen h-screen relative">
-        <div className="w-fit mx-auto absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center z-10">
+        <div className="w-fit mx-auto absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center z-10 ">
           <div
             className="image-wrapper mx-auto relative"
             style={{ width: "100vw", height: "20vmin" }}
@@ -50,6 +71,7 @@ export default function Home() {
         >
           <div className="bg-black opacity-30 absolute w-full h-full top-0 right-0 z-20"></div>
           <video
+            ref={videoRef}
             autoPlay
             muted
             loop
@@ -58,18 +80,6 @@ export default function Home() {
             className="w-full h-full object-cover absolute top-0 right-0 z-10"
           >
             <source src={"./lp-rain-2.mp4"} type="video/mp4" />
-          </video>
-
-          {/* video placeholder */}
-          <video
-            autoPlay
-            muted
-            loop
-            playsInline
-            preload="auto"
-            className="w-full h-full object-cover absolute top-0 right-0 z-5"
-          >
-            <source src={"./lp-rain-resized.mp4"} type="video/mp4" />
           </video>
         </div>
       </main>
