eacac430abd678b3fef339e0ebe12a0749752b73
gamalielara <aragamalielboanerges@gmail.com>
2023-06-24 15:37:18 +0700

    finalize blog grid ux

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -14,6 +14,7 @@ const Blogs: NextPage<BlogsPage> = ({ blogs }) => {
   const [showFeatured, setShowFeatured] = useState<boolean>(true);
   const [featuredBlogIndex, setFeaturedBlogIndex] = useState(0);
   const [maxBlogCardHeight, setMaxBlogCardHeight] = useState(0);
+  const [hoveredBlogIndex, setHoveredBlogIndex] = useState<number | null>(null);
 
   const featuredBlogs = blogs.filter((post) => post.featured);
 
@@ -28,6 +29,38 @@ const Blogs: NextPage<BlogsPage> = ({ blogs }) => {
 
     blogCardsRef.current.forEach((el) => {
       if (h < el.clientHeight) h = el.clientHeight;
+
+      const blogGroups = [
+        null,
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [10, 11, 12],
+        [13, 14, 15],
+      ];
+
+      el.onmouseenter = () => {
+        const idx = Number(el.className.match(/.*-(\d+)/)?.[1]) ?? null;
+        const group = Math.ceil(idx / 3);
+        const blogIdxs = blogGroups[group];
+
+        blogIdxs?.forEach((ix) => {
+          if (ix !== idx) {
+            console.info({ ix, idx });
+
+            blogCardsRef.current[ix - 1].className += " blog-card-hidden";
+          }
+        });
+
+        setHoveredBlogIndex(idx);
+      };
+
+      el.onmouseleave = () => {
+        setHoveredBlogIndex(null);
+        blogCardsRef.current.forEach(
+          (el) => (el.className = el.className.replace("blog-card-hidden", ""))
+        );
+      };
     });
 
     setMaxBlogCardHeight(h);
