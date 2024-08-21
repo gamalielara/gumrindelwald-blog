812cb2abf9ada41bc948fab4b88f3ff756ad8c6b
gamalielara <aragamalielboanerges@gmail.com>
2024-08-21 08:12:18 +0700

    create highlight card carousel

--- app/components/HighlightedBlogs/index.tsx
+++ app/components/HighlightedBlogs/index.tsx
@@ -10,31 +10,33 @@ const HighlightedBlogs: React.FC<Props> = ({ featuredBlogs }) => {
   return (
     <section className={styles["highlighted-blog-section"]}>
       <h3 className={styles["highlighted-blogs-title"]}>Highlighted Blogs</h3>
-      <ul className={styles["featured-blogs-list"]}>
-        {featuredBlogs.map((blog) => {
-          const {
-            thumbnail_image,
-            category,
-            excerpt,
-            created_at,
-            slug,
-            title,
-          } = blog;
+      <div className={styles["featured-blogs-container"]}>
+        <ul className={styles["featured-blogs-list"]}>
+          {featuredBlogs.map((blog) => {
+            const {
+              thumbnail_image,
+              category,
+              excerpt,
+              created_at,
+              slug,
+              title,
+            } = blog;
 
-          return (
-            <li className={styles["featured-blog-card"]} key={title + slug}>
-              <FeaturedBlogCard
-                thumbnail_image={thumbnail_image}
-                category={category}
-                excerpt={excerpt}
-                created_at={created_at}
-                slug={slug}
-                title={title}
-              />
-            </li>
-          );
-        })}
-      </ul>
+            return (
+              <li className={styles["featured-blog-card"]} key={title + slug}>
+                <FeaturedBlogCard
+                  thumbnail_image={thumbnail_image}
+                  category={category}
+                  excerpt={excerpt}
+                  created_at={created_at}
+                  slug={slug}
+                  title={title}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </section>
   );
 };
