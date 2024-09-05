5480a06921f02e7eb44acf04dcdbad62116ab5a8
gamalielara <aragamalielboanerges@gmail.com>
2024-09-05 07:51:05 +0700

    add logo animation

--- app/components/ColorThemeToggleButton/index.tsx
+++ app/components/ColorThemeToggleButton/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { setColorTheme } from "../../actions";
+import { setColorTheme } from "<app>/actions";
 import styles from "./styles.module.scss";
+import moon from "<assets>/svg/moon.svg";
+import sun from "<assets>/svg/sun.svg";
+import Image from "next/image";
 
 const ColorThemeToggleButton = () => {
   return (
-    <button
-      className={styles["toggle-btn"]}
-      onClick={() => setColorTheme()}
-      data-theme={""}
-    ></button>
+    <button className={styles["toggle-btn"]} onClick={() => setColorTheme()}>
+      <div>
+        <Image src={moon} alt="dark-mode-icon" width={20} height={20} />
+        <Image src={sun} alt="light-mode-icon" width={20} height={20} />
+      </div>
+    </button>
   );
 };
 
