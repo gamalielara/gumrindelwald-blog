381cad5765162c0d67f696141d8953471cf4ebde
gamalielara <aragamalielboanerges@gmail.com>
2024-09-07 06:43:22 +0700

    adjust toggle button in small device

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
-import React, { Suspense } from "react";
+import React from "react";
 import whiteLogoImage from "<assets>/com-white.png";
 import blackLogoImage from "<assets>/com-black.png";
 import {
@@ -17,60 +17,58 @@ const Footer: React.FC = async () => {
   const themeColorCookieValue = await getColorTheme();
 
   return (
-    <Suspense fallback={<>Loading...</>}>
-      <footer className={styles["footer"]}>
-        <div className={styles["footer-wrapper"]}>
-          <Image
-            src={
-              themeColorCookieValue === AppThemeColor.DUNKEL
-                ? whiteLogoImage
-                : blackLogoImage
-            }
-            alt="gumrindelwald.com"
-            layout="responsive"
+    <footer className={styles["footer"]}>
+      <div className={styles["footer-wrapper"]}>
+        <Image
+          src={
+            themeColorCookieValue === AppThemeColor.DUNKEL
+              ? whiteLogoImage
+              : blackLogoImage
+          }
+          alt="gumrindelwald.com"
+          layout="responsive"
+        />
+      </div>
+
+      <span className={styles["footer-desc"]}>
+        gumrindelwald v{process.env.REACT_APP_VERSION ?? "3.0"}. Copyright
+        &copy;
+        {thisYear}. This website is fully created by
+        <a href="https://gamalielara.com" target="_blank" rel="noreferrer">
+          Ara Gamaliel Boanerges
+        </a>
+      </span>
+
+      <div className={styles["social-media"]}>
+        <a
+          href="https://www.instagram.com/gamalielboanerges/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faInstagram}
+            color={colorTheme[themeColorCookieValue].textColor}
           />
-        </div>
+        </a>
 
-        <span className={styles["footer-desc"]}>
-          gumrindelwald v{process.env.REACT_APP_VERSION ?? "3.0"}. Copyright
-          &copy;
-          {thisYear}. This website is fully created by
-          <a href="https://gamalielara.com" target="_blank" rel="noreferrer">
-            Ara Gamaliel Boanerges
-          </a>
-        </span>
-
-        <div className={styles["social-media"]}>
-          <a
-            href="https://www.instagram.com/gamalielboanerges/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FontAwesomeIcon
-              icon={faInstagram}
-              color={colorTheme[themeColorCookieValue].textColor}
-            />
-          </a>
-
-          <a
-            href="https://www.linkedin.com/in/aragamaliel/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FontAwesomeIcon
-              icon={faLinkedin}
-              color={colorTheme[themeColorCookieValue].textColor}
-            />
-          </a>
-          <a href="https://gamalielara.com" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon
-              icon={faInternetExplorer}
-              color={colorTheme[themeColorCookieValue].textColor}
-            />
-          </a>
-        </div>
-      </footer>
-    </Suspense>
+        <a
+          href="https://www.linkedin.com/in/aragamaliel/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            color={colorTheme[themeColorCookieValue].textColor}
+          />
+        </a>
+        <a href="https://gamalielara.com" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon
+            icon={faInternetExplorer}
+            color={colorTheme[themeColorCookieValue].textColor}
+          />
+        </a>
+      </div>
+    </footer>
   );
 };
 
