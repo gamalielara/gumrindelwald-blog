26f51e78e5564f61c9b5d48cea3fb43e5c228492
gamalielara <aragamalielboanerges@gmail.com>
2024-10-12 15:27:09 +0700

    extract post comment form

---
+++ app/components/CommentsSection/PostCommentForm/index.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import React, { MouseEventHandler, useRef } from "react";
+import styles from "./styles.module.scss";
+
+const PostCommentForm = () => {
+  const usernameRef = useRef<string>();
+  const emailRef = useRef<string>();
+  const bodyRef = useRef<string>();
+
+  const submitCommentHandler: MouseEventHandler<HTMLInputElement> = async (
+    e
+  ) => {
+    e.preventDefault();
+
+    if (!usernameRef.current || !bodyRef.current) {
+      // TODO: show toast validate username and body
+      return;
+    }
+
+    if (
+      emailRef.current &&
+      !emailRef.current.match(
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      )
+    ) {
+      // TODO: show toast Validate valid email
+      return;
+    }
+
+    console.log("HAIII ", {
+      username: usernameRef.current,
+      email: emailRef.current,
+      body: bodyRef.current,
+    });
+  };
+
+  return (
+    <section className={styles["post-comment-form"]}>
+      <form className={styles["comment-form"]}>
+        <h4>Post your comment here</h4>
+        <input
+          required
+          type="text"
+          placeholder="Enter your name here"
+          className={styles["comment-section-input"]}
+          onChange={(e) => {
+            usernameRef.current = e.target.value;
+          }}
+        />
+        <input
+          placeholder="Enter your email (optional, fill this if you want to get email notification if someone replies.)"
+          type="email"
+          className={styles["comment-section-input"]}
+          onChange={(e) => {
+            emailRef.current = e.target.value;
+          }}
+        />
+        <textarea
+          required
+          placeholder="Enter your comment here..."
+          className={styles["insert-comment-field"]}
+          onChange={(e) => {
+            bodyRef.current = e.target.value;
+          }}
+        />
+        <input
+          className={styles["submit-comment-btn"]}
+          type="submit"
+          value="Post Comment"
+          onClick={submitCommentHandler}
+        />
+      </form>
+    </section>
+  );
+};
+
+export default PostCommentForm;
--- app/components/CommentsSection/index.tsx
+++ app/components/CommentsSection/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./styles.module.scss";
 import CommentCard from "./CommentCard";
 import { Comment } from "<utils>/types";
+import PostCommentForm from "./PostCommentForm";
 
 interface Props {
   comments: Array<Comment>;
@@ -23,31 +24,7 @@ const CommentsSection: React.FC<Props> = ({ comments }) => {
           />
         ))}
       </section>
-
-      <form className={styles["comment-form"]}>
-        <h4>Post your comment here</h4>
-        <input
-          required
-          type="text"
-          placeholder="Enter your name here"
-          className={styles["comment-section-input"]}
-        />
-        <input
-          placeholder="Enter your email (optional, fill this if you want to get email notification if someone replies.)"
-          type="email"
-          className={styles["comment-section-input"]}
-        />
-        <textarea
-          required
-          placeholder="Enter your comment here..."
-          className={styles["insert-comment-field"]}
-        />
-        <input
-          className={styles["submit-comment-btn"]}
-          type="submit"
-          value="Post Comment"
-        />
-      </form>
+      <PostCommentForm />
     </>
   );
 };
